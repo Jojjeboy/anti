9b9d5c5dce45a54e0b02d04a81bc26e80a6e6e71
Johannes Kärnstam <johannes.karnstam@vgregion.se>
2026-05-19 09:36:53 +0200

    Fixed statistics

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/looplist/',
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg', 'favicon.png'],
